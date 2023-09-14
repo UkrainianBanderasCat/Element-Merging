a0be75baf085dc8bfeea4a47dfebaa3b4db1ff3e
UkrainianBanderasCat <andreyev.ura@gmail.com>
2023-09-14 23:27:50 +0300

    smol changes

--- Assets/Scripts/Managers/ElementManager.cs
+++ Assets/Scripts/Managers/ElementManager.cs
@@ -112,18 +112,25 @@ public class ElementManager : MonoBehaviour
         }
     }
 
-    public static Sprite LoadNewSprite(string FilePath, float PixelsPerUnit = 16f, SpriteMeshType spriteType = SpriteMeshType.Tight)
+    public Sprite LoadNewSprite(string FilePath, float PixelsPerUnit = 16f, SpriteMeshType spriteType = SpriteMeshType.Tight)
     {
 
         // Load a PNG or JPG image from disk to a Texture2D, assign this texture to a new sprite and return its reference
 
         Texture2D SpriteTexture = LoadTexture(FilePath);
+
+        if (SpriteTexture == null)
+        {
+            Debug.Log("Failed to load texture with path: " + FilePath);
+            return null;
+        }
+
         Sprite NewSprite = Sprite.Create(SpriteTexture, new Rect(0, 0, SpriteTexture.width, SpriteTexture.height), new Vector2(0, 0), PixelsPerUnit, 0, spriteType);
 
         return NewSprite;
     }
     
-    public static Texture2D LoadTexture(string FilePath)
+    public Texture2D LoadTexture(string FilePath)
     {
 
         // Load a PNG or JPG file from disk to a Texture2D
